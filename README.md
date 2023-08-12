521fbf82d9e8f274df676984db5de4d821cd1833
Emerson <63008698+emerso-n@users.noreply.github.com>
2023-08-12 03:38:26 -0400

    finished lol

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
-Mountain: Photo by <a href="https://unsplash.com/@withluke?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Luke Stackpoole</a> on <a href="https://unsplash.com/photos/ej8pKvHDg_c?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+#Restaurant Site
 
-Trees: Photo by Felipe Alves from Pexels: https://www.pexels.com/photo/green-trees-near-body-of-water-4077937/
+Photo by <a href="https://unsplash.com/@ninjason?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Jason Leung</a> on <a href="https://unsplash.com/photos/poI7DelFiVA?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+
+Photo by <a href="https://unsplash.com/@jaywennington?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Jay Wennington</a> on <a href="https://unsplash.com/photos/N_Y88TWmGwA?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
--- src/index.js
+++ src/index.js
@@ -6,13 +6,74 @@ import './css/style.css'
 const pageCon = CreateElement(document.body, { id: 'page-con' })
 
 const main = CreateElement(pageCon, { id: 'main' })
+const pageheader = CreateElement(main, { id: 'page-header' })
+CreateElement(pageheader, { classList: 'header-stripe' })
+CreateElement(pageheader, { innerHTML: 'Restaurant', id: 'logo' })
+CreateElement(pageheader, { classList: 'header-stripe' })
 
-const sidebar = CreateElement(main, { id: 'sidebar' })
-CreateElement(sidebar, { innerHTML: 'The Flying Fan', id: 'logo' })
-CreateElement(sidebar, { type: 'button', innerHTML: 'Home', classList: 'btn-active' })
-CreateElement(sidebar, { type: 'button', innerHTML: 'Menu' })
-CreateElement(sidebar, { type: 'button', innerHTML: 'About' })
-
-CreateElement(main, { id: 'main-content', innerHTML: 'Here is some text for the content on the homepage' })
-
+const navbtns = CreateElement(main, { type: 'nav', id: 'nav' })
+const homebtn = CreateElement(navbtns, { type: 'button', innerHTML: 'Home', classList: 'btn-active' })
+const menubtn = CreateElement(navbtns, { type: 'button', innerHTML: 'Menu' })
+const aboutusbtn = CreateElement(navbtns, { type: 'button', innerHTML: 'About Us' })
 CreateElement(pageCon, { type: 'footer', innerHTML: 'A website by emerso-n (2023)' })
+
+const navs = [homebtn, menubtn, aboutusbtn]
+navs.forEach(element => {
+  element.addEventListener('click', changepage)
+})
+
+function changepage (e) {
+  maincontent.remove()
+  navs.forEach(element => {
+    element.classList.remove('btn-active')
+  })
+  switch (e.target.innerHTML) {
+    case 'Home':
+      home()
+      break
+    case 'Menu':
+      menu()
+      break
+    case 'About Us':
+      aboutus()
+      break
+
+    default:
+      break
+  }
+  e.target.classList = 'btn-active'
+}
+
+let maincontent
+function home () {
+  maincontent = CreateElement(main, { id: 'main-content', classList: 'main-homepage' })
+  homebtn.classList = 'btn-active'
+  const textcon = CreateElement(maincontent, { id: 'text_con' })
+  CreateElement(maincontent, { id: 'fill_con' })
+  CreateElement(textcon, { type: 'h1', innerHTML: 'This is: Restaurant:' })
+  CreateElement(textcon, { type: 'p', innerHTML: 'We serve Food' })
+  CreateElement(textcon, { type: 'p', innerHTML: 'Here at Restaurant, we are committed to making food and giving it to you for you to eat. We strive to fullfil every order we receive within a timely manner and to a quality that is acceptably good-tasting and will not harm the patron. We promise that it will be a pleasant experience so come and try for yourself.' })
+}
+home()
+
+const menuitems = [['Water', 'Drink some water / 3.00'], ['Bread', 'Made from wheat / 5.00'], ['Sandwich', 'Made from bread / 8.00'], ['Tacos', 'Has real meat it in / 12.00'], ['Cake', 'Good for birthdays / 6.00'], ['Ice Cream', 'It\'s delicious / 5.00']]
+function menu () {
+  menubtn.classList = 'btn-active'
+  maincontent = CreateElement(main, { id: 'main-content', classList: 'main-menu' })
+  CreateElement(maincontent, { classList: 'menu-side' })
+  const menucon = CreateElement(maincontent, { id: 'menu-items_con' })
+  for (let i = 0; i < menuitems.length; i++) {
+    const menuitem = CreateElement(menucon, { classList: 'menu-item' })
+    CreateElement(menuitem, { classList: 'item-name', innerHTML: menuitems[i][0] })
+    CreateElement(menuitem, { classList: 'item-desc', innerHTML: menuitems[i][1] })
+  }
+  CreateElement(maincontent, { classList: 'menu-side' })
+}
+// menu()
+
+function aboutus () {
+  aboutusbtn.classList = 'btn-active'
+  maincontent = CreateElement(main, { id: 'main-content', classList: 'aboutus-main' })
+  CreateElement(maincontent, { id: 'aboutus-text', innerHTML: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo ci dont feel like writing thisonsequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.' })
+}
+// aboutus()
